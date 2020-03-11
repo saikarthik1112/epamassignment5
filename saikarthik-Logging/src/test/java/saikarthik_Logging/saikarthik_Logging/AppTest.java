e6ccbed919d5a44b1ff2fc5e8fd7875e6d805df0
saikarthik <saikarthik.b007@gmail.com>
2020-03-11 22:18:10 +0530

    Adding features

---
+++ saikarthik-Logging/src/test/java/saikarthik_Logging/saikarthik_Logging/AppTest.java
@@ -0,0 +1,20 @@
+package saikarthik_Logging.saikarthik_Logging;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
